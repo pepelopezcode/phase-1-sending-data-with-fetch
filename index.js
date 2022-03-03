7a48c6eb2b4a0b1908e61b4432a2c1aaa529a817
pepe lopez <plopez741@gmail.com>
2022-03-02 20:13:37 -0800

    commit tree ed6b31c171517b6a8b52da5cabb51b5dd2aad1d9

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+/* 
+function submit data (2 arg) user name, user email
+*/
+function submitData(userName, userEmail){
+    const userInfo = {
+        name: userName,
+        email: userEmail,
+    }
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then(resp => resp.json())
+    .then(data => document.querySelector('body').append(data.id))
+    .catch(error => document.querySelector('body').append(error.message))
+}
